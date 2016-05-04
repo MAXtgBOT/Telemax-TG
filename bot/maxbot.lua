1c415df7f0349bca2af292f777a06626d28468ab
MAXtgBOT <xyzbot002@gmail.com>
2016-05-04 23:24:49 +0430

    Update maxbot.lua

--- bot/maxbot.lua
+++ bot/maxbot.lua
@@ -228,7 +228,8 @@ function create_config( )
     "echo",
     "weather",
     "activeuser",
-    "save"
+    "save",
+    "autoleave"
     },
     sudo_users = {205903314,201644269},--Sudo users
     disabled_channels = {},
