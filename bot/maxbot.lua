0f25b530bd958e0dd824d473f6a183fc2be548e2
MAXtgBOT <xyzbot002@gmail.com>
2016-05-04 20:47:35 +0430

    Update and rename seedbot.lua to maxbot.lua

--- bot/maxbot.lua
+++ bot/maxbot.lua
@@ -6,7 +6,7 @@ require("./bot/utils")
 
 VERSION = '2'
 
--- This function is called when tg receive a msg
+— This function is called when tg receive a msg
 function on_msg_receive (msg)
   if not started then
     return
@@ -15,7 +15,7 @@ function on_msg_receive (msg)
   local receiver = get_receiver(msg)
   print (receiver)
 
-  --vardump(msg)
+  —vardump(msg)
   msg = pre_process_service_msg(msg)
   if msg_valid(msg) then
     msg = pre_process_msg(msg)
@@ -39,19 +39,19 @@ function on_binlog_replay_end()
 
   _config = load_config()
 
-  -- load plugins
+  — load plugins
   plugins = {}
   load_plugins()
 end
 
 function msg_valid(msg)
-  -- Don't process outgoing messages
+  — Don't process outgoing messages
   if msg.out then
     print('\27[36mNot valid: msg from us\27[39m')
     return false
   end
 
-  -- Before bot was started
+  — Before bot was started
   if msg.date < now then
     print('\27[36mNot valid: old msg\27[39m')
     return false
@@ -83,22 +83,22 @@ function msg_valid(msg)
   end
 
   if msg.from.id == 777000 then
-  	local login_group_id = 1
-  	--It will send login codes to this chat
+    local login_group_id = 1
+    —It will send login codes to this chat
     send_large_msg('chat#id'..login_group_id, msg.text)
   end
 
   return true
 end
 
---
+—
 function pre_process_service_msg(msg)
    if msg.service then
       local action = msg.action or {type=""}
-      -- Double ! to discriminate of normal actions
+      — Double ! to discriminate of normal actions
       msg.text = "!!tgservice " .. action.type
 
-      -- wipe the data to allow the bot to read service messages
+      — wipe the data to allow the bot to read service messages
       if msg.out then
          msg.out = false
       end
@@ -109,7 +109,7 @@ function pre_process_service_msg(msg)
    return msg
 end
 
--- Apply plugin.pre_process function
+— Apply plugin.pre_process function
 function pre_process_msg(msg)
   for name,plugin in pairs(plugins) do
     if plugin.pre_process and msg then
@@ -121,19 +121,19 @@ function pre_process_msg(msg)
   return msg
 end
 
--- Go over enabled plugins patterns.
+— Go over enabled plugins patterns.
 function match_plugins(msg)
   for name, plugin in pairs(plugins) do
     match_plugin(plugin, name, msg)
   end
 end
 
--- Check if plugin is on _config.disabled_plugin_on_chat table
+— Check if plugin is on _config.disabled_plugin_on_chat table
 local function is_plugin_disabled_on_chat(plugin_name, receiver)
   local disabled_chats = _config.disabled_plugin_on_chat
-  -- Table exists and chat has disabled plugins
+  — Table exists and chat has disabled plugins
   if disabled_chats and disabled_chats[receiver] then
-    -- Checks if plugin is disabled on this chat
+    — Checks if plugin is disabled on this chat
     for disabled_plugin,disabled in pairs(disabled_chats[receiver]) do
       if disabled_plugin == plugin_name and disabled then
         local warning = 'Plugin '..disabled_plugin..' is disabled on this chat'
@@ -149,18 +149,18 @@ end
 function match_plugin(plugin, plugin_name, msg)
   local receiver = get_receiver(msg)
 
-  -- Go over patterns. If one matches it's enough.
+  — Go over patterns. If one matches it's enough.
   for k, pattern in pairs(plugin.patterns) do
     local matches = match_pattern(pattern, msg.text)
     if matches then
       print("msg matches: ", pattern)
 
-      if is_plugin_disabled_on_chat(plugin_name, receiver) then
+if is_plugin_disabled_on_chat(plugin_name, receiver) then
         return nil
       end
-      -- Function exists
+      — Function exists
       if plugin.run then
-        -- If plugin is for privileged users only
+        — If plugin is for privileged users only
         if not warns_user_not_allowed(plugin, msg) then
           local result = plugin.run(msg, matches)
           if result then
@@ -168,28 +168,28 @@ function match_plugin(plugin, plugin_name, msg)
           end
         end
       end
-      -- One patterns matches
+      — One patterns matches
       return
     end
   end
 end
 
--- DEPRECATED, use send_large_msg(destination, text)
+— DEPRECATED, use send_large_msg(destination, text)
 function _send_msg(destination, text)
   send_large_msg(destination, text)
 end
 
--- Save the content of _config to config.lua
+— Save the content of _config to config.lua
 function save_config( )
   serialize_to_file(_config, './data/config.lua')
   print ('saved config into ./data/config.lua')
 end
 
--- Returns the config from config.lua file.
--- If file doesn't exist, create it.
+— Returns the config from config.lua file.
+— If file doesn't exist, create it.
 function load_config( )
   local f = io.open('./data/config.lua', "r")
-  -- If config.lua doesn't exist
+  — If config.lua doesn't exist
   if not f then
     print ("Created new config file: data/config.lua")
     create_config()
@@ -203,9 +203,9 @@ function load_config( )
   return config
 end
 
--- Create a basic config.json file and saves it.
+— Create a basic config.json file and saves it.
 function create_config( )
-  -- A simple config with basic plugins and ourselves as privileged user
+  — A simple config with basic plugins and ourselves as privileged user
   config = {
     enabled_plugins = {
     "onservice",
@@ -226,30 +226,23 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {205903314,201644269},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
+    about_text = [[TeleMax v1 - Open Source
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
+https://github.com/MAXtgBOT/Telemax-TG
 
 Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
+rastin(@goraze)
+ramin(@raminoa)
 
-Special thanks to:
-Juan Potato
-Siyanew
-Topkecleon
-Vamptacus
-
-Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
+special thanks to:
+saeed
+mehran
+amin
+shaho
 ]],
     help_text_realm = [[
 Realm Commands:
@@ -438,7 +431,7 @@ function on_our_id (id)
 end
 
 function on_user_update (user, what)
-  --vardump (user)
+  —vardump (user)
 end
 
 function on_chat_update (chat, what)
@@ -446,13 +439,13 @@ function on_chat_update (chat, what)
 end
 
 function on_secret_chat_update (schat, what)
-  --vardump (schat)
+  —vardump (schat)
 end
 
 function on_get_difference_end ()
 end
 
--- Enable plugins in config.json
+— Enable plugins in config.json
 function load_plugins()
   for k, v in pairs(_config.enabled_plugins) do
     print("Loading plugin", v)
@@ -472,45 +465,45 @@ function load_plugins()
 end
 
 
--- custom add
+— custom add
 function load_data(filename)
 
-	local f = io.open(filename)
-	if not f then
-		return {}
-	end
-	local s = f:read('*all')
-	f:close()
-	local data = JSON.decode(s)
+  local f = io.open(filename)
+  if not f then
+    return {}
+  end
+  local s = f:read('*all')
+  f:close()
+  local data = JSON.decode(s)
 
-	return data
+  return data
 
 end
 
 function save_data(filename, data)
 
-	local s = JSON.encode(data)
-	local f = io.open(filename, 'w')
-	f:write(s)
-	f:close()
+  local s = JSON.encode(data)
+  local f = io.open(filename, 'w')
+  f:write(s)
+  f:close()
 
 end
 
--- Call and postpone execution for cron plugins
+— Call and postpone execution for cron plugins
 function cron_plugins()
 
   for name, plugin in pairs(plugins) do
-    -- Only plugins with cron function
+    — Only plugins with cron function
     if plugin.cron ~= nil then
       plugin.cron()
     end
   end
 
-  -- Called again in 2 mins
+  — Called again in 2 mins
   postpone (cron_plugins, false, 120)
 end
 
--- Start and load values
+— Start and load values
 our_id = 0
 now = os.time()
 math.randomseed(now)
