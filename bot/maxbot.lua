35f823f805827fbec9a557b50a7a04a90e783fc8
MAXtgBOT <xyzbot002@gmail.com>
2016-05-05 10:16:57 +0430

    Update maxbot.lua

--- bot/maxbot.lua
+++ bot/maxbot.lua
@@ -235,7 +235,8 @@ function create_config( )
     "giphy",
     "hello",
     "plugins",
-    "linkpv"
+    "linkpv",
+    "info"
     },
     sudo_users = {205903314,201644269},--Sudo users
     disabled_channels = {},
