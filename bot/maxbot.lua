0323f04dc2aa90459741d27e6e020b0a51531f70
MAXtgBOT <xyzbot002@gmail.com>
2016-05-05 12:32:12 +0430

    Update maxbot.lua

--- bot/maxbot.lua
+++ bot/maxbot.lua
@@ -240,7 +240,8 @@ function create_config( )
     "bts",
     "anti-bot",
     "bugzilla",
-    "channels"
+    "channels",
+    "anti-flood"
     },
     sudo_users = {205903314,201644269},--Sudo users
     disabled_channels = {},
