031d53ec5d1f7b7a936bdac477a45b2fbc2a7a60
MAXtgBOT <xyzbot002@gmail.com>
2016-05-05 10:08:16 +0430

    Update maxbot.lua

--- bot/maxbot.lua
+++ bot/maxbot.lua
@@ -234,7 +234,8 @@ function create_config( )
     "hackernews",
     "giphy",
     "hello",
-    "plugins"
+    "plugins",
+    "linkpv"
     },
     sudo_users = {205903314,201644269},--Sudo users
     disabled_channels = {},
