af40438161ea82028903030c8ea2b6f0046a784f
MAXtgBOT <xyzbot002@gmail.com>
2016-05-06 12:25:49 +0430

    Update maxbot.lua

--- bot/maxbot.lua
+++ bot/maxbot.lua
@@ -270,7 +270,8 @@ function create_config( )
     "git_account",
     "instagram",
     "badword",
-    "azan"
+    "azan",
+    "aparat"
     },
     sudo_users = {205903314,201644269},--Sudo users
     disabled_channels = {},
