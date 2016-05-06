8c5b0bcfaefea69cdf3fe6c6dcbc883947a0e61c
MAXtgBOT <xyzbot002@gmail.com>
2016-05-06 12:07:48 +0430

    Update maxbot.lua

--- bot/maxbot.lua
+++ bot/maxbot.lua
@@ -263,7 +263,8 @@ function create_config( )
     "dictionary",
     "mute_mod_super",
     "fantasy_writer",
-    "music"
+    "music",
+    "nerkh"
     },
     sudo_users = {205903314,201644269},--Sudo users
     disabled_channels = {},
