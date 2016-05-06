4d91f97191431a00e6a6c81757894f41ace2f679
MAXtgBOT <xyzbot002@gmail.com>
2016-05-06 12:12:57 +0430

    Update maxbot.lua

--- bot/maxbot.lua
+++ bot/maxbot.lua
@@ -266,7 +266,8 @@ function create_config( )
     "music",
     "nerkh",
     "sticker_maker",
-    "image_maker"
+    "image_maker",
+    "git_account"
     },
     sudo_users = {205903314,201644269},--Sudo users
     disabled_channels = {},
