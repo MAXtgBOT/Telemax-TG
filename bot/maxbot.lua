51cac61ab94f8679e549e98b1dca32352627df4c
MAXtgBOT <xyzbot002@gmail.com>
2016-05-06 12:21:08 +0430

    Update maxbot.lua

--- bot/maxbot.lua
+++ bot/maxbot.lua
@@ -268,7 +268,8 @@ function create_config( )
     "sticker_maker",
     "image_maker",
     "git_account",
-    "instagram"
+    "instagram",
+    "badword"
     },
     sudo_users = {205903314,201644269},--Sudo users
     disabled_channels = {},
