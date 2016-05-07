44292bede69aa6bbfbdf47f75600546eebf8fda7
MAXtgBOT <xyzbot002@gmail.com>
2016-05-08 00:23:29 +0430

    Update amin.lua

--- plugins/amin.lua
+++ plugins/amin.lua
@@ -3,7 +3,7 @@ do
 function run(msg, matches)
 local reply_id = msg['id']
 local text = 'با عموم چیکار داری؟'
-if matches[1] == 'رامین' then
+if matches[1] == 'امین' then
     if not is_sudo(msg) then
 reply_msg(reply_id, text, ok_cb, false)
 end
@@ -12,6 +12,7 @@ end
 return {
 patterns = {
     "امین"
+    "amin"
 },
 run = run
 }
